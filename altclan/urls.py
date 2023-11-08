353764247737634188adc71edd25108975bc629d
Mofe Odujirin <thebaggieboy@protonmail.com>
2023-11-08 11:43:37 +0100

    d

--- altclan/urls.py
+++ altclan/urls.py
@@ -25,13 +25,13 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('', include('rest_framework.urls', namespace='rest_framework')),
-    #path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    #path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
-    #path('api/token/verify/', jwt_views.TokenVerifyView.as_view(), name='token_verify'),
+    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', jwt_views.TokenVerifyView.as_view(), name='token_verify'),
     
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
-    #path('dj-rest-auth/google/', GoogleLogin.as_view(), name='google_login')
+    path('dj-rest-auth/google/', GoogleLogin.as_view(), name='google_login')
   
   
 
