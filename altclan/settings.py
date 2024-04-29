d1eebefd0ae29849889044aacb7b527eef3ad14f
Mofe Odujirin <thebaggieboy@protonmail.com>
2024-04-29 09:03:02 +0100

    corrected trusted origins

--- altclan/settings.py
+++ altclan/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'altclan.store', 'altclanstore.vercel.app' 'altclan-api-v1.onrender.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'altclan.store', 'altclanstore.vercel.app', 'altclan-api-v1.onrender.com']
 
 
 # Application definition
