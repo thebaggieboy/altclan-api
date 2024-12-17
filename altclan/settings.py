79508b6114b578082460c4753b47d533f607a331
Mofe Odujirin <thebaggieboy@protonmail.com>
2024-12-16 21:52:15 -0800

    sduebuh

--- altclan/settings.py
+++ altclan/settings.py
@@ -233,20 +233,20 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:8000',
     'http://altclan.com',
     'https://altclan.com',
-    'https://altclan-api-v1.onrender.com',
-    'https://altclan-brands-api-1-1.onrender.com',
+    'https://altclan-api.onrender.com',
     'https://api.cloudinary.com'
     
 ]
 
 CORS_ORIGIN_WHITELIST = [
+    
     'http://localhost:3000',
     'http://127.0.0.1:3000',
     'http://localhost:8000',
     'http://127.0.0.1:8000',
     'http://altclan.com',
     'https://altclan.com',
-    'https://altclan-api-v1.onrender.com',
+    'https://altclan-api.onrender.com',
     'https://api.cloudinary.com'
     
     
