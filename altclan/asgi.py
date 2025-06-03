d87ddbd6e6a83a489e997582fb5bac6bb1acb0c9
Mofe Odujirin <thebaggieboy@protonmail.com>
2025-06-03 06:25:52 -0400

    nmn

--- altclan/asgi.py
+++ altclan/asgi.py
@@ -1,16 +1,22 @@
-"""
-ASGI config for altclan project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
-"""
-
 import os
-
 from django.core.asgi import get_asgi_application
 
+# Set Django settings module FIRST
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'altclan.settings')
 
-application = get_asgi_application()
+# Initialize Django ASGI app BEFORE importing dependencies that require Django
+django_asgi_app = get_asgi_application()
+
+# Now import Channels and routing
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from notifications import routing  # Now safe to import
+
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,  # Use the pre-loaded ASGI app
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            routing.websocket_urlpatterns
+        )
+    ),
+})
