c1d4a860d727c722076de5456b56fb7355bdc2bf
Mofe Odujirin <thebaggieboy@protonmail.com>
2025-06-05 02:52:02 -0400

    made more changes to dashboard, by linking the create pages and list pages, adding api creation functions

--- altclan/settings.py
+++ altclan/settings.py
@@ -266,6 +266,7 @@ EMAIL_HOST = os.getenv("EMAIL_HOST")
 EMAIL_PORT = os.getenv("EMAIL_PORT")
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+
 EMAIL_USE_TLS = True
 
 # Detailed logging configuration
