4796f2a3d0ca5b9bdab787eb36c885b2f7ae1cea
Mofe Odujirin <thebaggieboy@protonmail.com>
2025-06-05 09:07:50 -0400

    made more changes to dashboard, by linking the create pages and list pages, adding api creation functions

--- notifications/serializers.py
+++ notifications/serializers.py
@@ -4,5 +4,6 @@ from .models import *
 class NotificationSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Notification
-        fields = ['id', 'user', 'sender', 'notification_type']   
+        fields = ['id', 'user', 'sender', 'notification_type', 
+                  'message', 'is_read', 'created_at', 'target_url']   
 
