bec891f4347c9a73ef8f8be83208a6bf7768fad7
Mofe Odujirin <thebaggieboy@protonmail.com>
2025-06-05 04:17:06 -0400

    made more changes to dashboard, by linking the create pages and list pages, adding api creation functions

--- notifications/signals.py
+++ notifications/signals.py
@@ -23,7 +23,7 @@ def create_new_user_notification(sender, instance, created, **kwargs):
         )
         
         # Send real-time notification
-        """channel_layer = get_channel_layer()
+        channel_layer = get_channel_layer()
         async_to_sync(channel_layer.group_send)(
             f"New account{instance.post.id}",
             {
@@ -37,4 +37,3 @@ def create_new_user_notification(sender, instance, created, **kwargs):
                 }
             }
         )
-"""
