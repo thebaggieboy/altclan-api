6b11dfdbcbc3b873c3d4d0f222bf9abf6dc9d613
Mofe Odujirin <thebaggieboy@protonmail.com>
2026-09-01 03:39:08 +0100

    urfrbfub

--- altclan/settings.py
+++ altclan/settings.py
@@ -270,7 +270,6 @@ EMAIL_HOST = os.getenv("EMAIL_HOST", "smtp.hostinger.com")
 EMAIL_PORT = int(os.getenv("EMAIL_PORT", "465"))
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'False').lower() == 'true'
 EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL', 'False').lower() == 'true'
 EMAIL_TIMEOUT = int(os.getenv("EMAIL_TIMEOUT", "10"))
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", EMAIL_HOST_USER or "noreply@altclan.shop")
